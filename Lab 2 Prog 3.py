33f2e20d951d9f4f21a2815573ffda0d272d2140
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-10-03 17:35:09 -0300

    Clase Fecha creada, prueba básica del programa
    PD: Anio sin ñ para que no explote GitHub

--- Lab 2 Prog 3.py
+++ Lab 2 Prog 3.py
@@ -1,7 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  3 15:52:13 2025
+import math
+import time
 
-@author: Enrique Sanchez
-"""
 
+# ---------------------------------------------------------
+#                 Creación de clases
+# ---------------------------------------------------------
+
+class Fecha:
+    def __init__(self, dia: int, mes: int, anio: int, hora: int, minuto: int):
+        self.dia = dia
+        self.mes = mes
+        self.anio = anio
+        self.hora = hora
+        self.minuto = minuto
+
+    def setFecha(self, dia: int, mes: int, anio: int, hora: int, minuto: int):
+        """ Actualiza la fecha y hora """
+        
+        self.dia = dia
+        self.mes = mes
+        self.anio = anio
+        self.hora = hora
+        self.minuto = minuto
+        
+    def getFechaString(self):
+        """ Devuelve una cadena con los valores de Fecha relacionados """
+        
+        return f"{self.dia}/{self.mes}/{self.anio} --> {self.hora}:{self.minuto}"
+    
+    def calcularEdad(self,anioNacimiento: int):
+        
+        return (self.anio - anioNacimiento)
+        
+    
+# ---------------------------------------------------------
+#                       PROGRAMA
+# ---------------------------------------------------------
+
+f = Fecha(3, 10, 2025, 16, 30)
+print(f.getFechaString())   # 03/10/2025 16:30
+#f.setFecha(1, 1, 2000, 0, 0)
+print(f.getFechaString())   # 01/01/2000 00:00
+print(f.calcularEdad(2003))        # Probando año de nacimiento
