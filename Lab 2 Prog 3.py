9480933523f1fee8429ed1ab18c199067d05f756
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-10-04 03:22:34 -0300

    Progreso

--- Lab 2 Prog 3.py
+++ Lab 2 Prog 3.py
@@ -29,10 +29,103 @@ class Fecha:
         return f"{self.dia}/{self.mes}/{self.anio} --> {self.hora}:{self.minuto}"
     
     def calcularEdad(self,anioNacimiento: int):
+        """ En base al año de nacimiento ingresado calcula la edad (sin meses) """
         
         return (self.anio - anioNacimiento)
         
     
+    
+# ---------------------------------------------------------
+
+class Persona:
+    def __init__(self, nombre: str, fechaNac: Fecha, CI: int, correo: str, codigo: str):
+        
+        self.nombre = nombre
+        self.fechaNac = fechaNac
+        self.CI = CI
+        self.correo = correo
+        self.codigo = codigo
+    
+    def getNombre(self):
+        return f"{self.nombre}"
+    
+    def getFechaNac(self):
+        return f"{self.fechaNac.getFechaString}"
+    
+    def getCI(self):
+        return f"{self.CI}"
+    
+    def getCorreo(self):
+        return f"{self.correo}"
+    
+    def getCodigo(self):
+        return f"{self.codigo}"
+    
+         # -------------- # 
+         
+    def setNombre(self, nombre: str):
+        self.nombre = nombre
+        
+    def setFechaNac(self, fechaNac: Fecha):
+        self.fechaNac = fechaNac
+        
+    def setCI(self, CI: int):
+        self.CI = CI
+        
+    def setCorreo(self, correo: str):
+        self.correo = correo
+        
+        
+        
+# ---------------------------------------------------------
+
+class Vendedor(Persona):
+    def __init__(self, nombre: str, fechaNac: Fecha, CI: int, correo: str, codigo: str,
+                 productos: list, fechaIngreso: Fecha, patrimonio: float):
+
+        # Llamado al constructor de Persona
+        super().__init__(nombre, fechaNac, CI, correo, codigo)
+
+        # Adición de atributos específicos de Vendedor
+        self.productos = productos        # Lista de objetos de Producto
+        self.fechaIngreso = fechaIngreso  # Un objeto de Fecha
+        self.patrimonio = patrimonio      # Saldo del vendedor
+        
+    def getProducts(self,productos: list):
+        return f"{self.productos}"
+    
+    def 
+
+
+
+# ---------------------------------------------------------
+
+class Cliente(Persona):    
+    def __init__(self, nombre: str, fechaNac: Fecha, CI: int, correo: str, codigo: str,
+                 carrito: list, fechaIngreso: Fecha, puntaje: int):
+
+        super().__init__(nombre, fechaNac, CI, correo, codigo)
+
+        self.carrito = carrito            # Lista de ítems de itemCompra
+        self.fechaIngreso = fechaIngreso
+        self.puntaje = puntaje
+
+
+
+# ---------------------------------------------------------
+
+class Compra:
+    def __init__ (self, cliente: Cliente, fecha: Fecha, listado: list, modoPago: int, numeroFactura: int, codigo: str, estado: int):
+        self.cliente = cliente
+        self.fecha = fecha
+        self.listado = listado
+        self.modoPago = modoPago
+        self.numeroFactura = numeroFactura
+        self.codigo = codigo
+        self.estado = estado
+        
+    def getCliente(self, cliente: Cliente):
+        return f"{self.cliente}"
 # ---------------------------------------------------------
 #                       PROGRAMA
 # ---------------------------------------------------------
